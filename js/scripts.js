d92be69d025350350284b381c41d9e424f47710f
Jose Amesquita and Jamison Cozart <jamison@>
2020-01-13 10:26:50 -0800

    1. add the following functions bmi, convertToCelsius, convertToFahrenheit, and gallonsToLiters

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,22 @@ var divide = function(number1, number2){
   return number1 / number2;
 };
 
+var bmi = function(height, weight){
+  return weight / (height*height);
+};
+
+var convertToFahrenheit = function(celcius) {
+return (celcius*9)/5 + 32;
+}
+
+var convertToCelcius = function(fahrenheit) {
+return ((fahrenheit-32)*5)/9;
+}
+
+var gallonsToLiters = function(gallons){
+return (gallons / 0.264);
+};
+
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
 
