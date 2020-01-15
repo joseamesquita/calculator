5312ef86c8647b90184aef10c6997522305c4aec
Jose Amesquita and Kristina Hengster Tintor <jose@>
2020-01-15 11:37:26 -0800

    1.add features to the calculator app

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -30,10 +32,41 @@ var gallonsToLiters = function(gallons){
 return (gallons / 0.264);
 };
 
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
+// User interface (or front-end) logic:
 
-alert(add(number1, number2));
-alert(subtract(number1, number2));
-alert(multiply(number1, number2));
-alert(divide(number1, number2));
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    // alert(add(number1, number2));
+    var result = add(number1, number2); //removes the alert box
+    $("#output").text(result).toggle();
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    // alert(add(number1, number2));
+    var result = subtract(number1, number2); //removes the alert box
+    $("#output2").text(result).toggle();
+  });
+
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result).toggle();
+  });
+
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result).toggle();
+  });
+
+});
